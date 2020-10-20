50bee925430f68823b9c9220709e17537e463d4f
Tinyunshe <40223445+Tinyunshe@users.noreply.github.com>
2020-10-20 14:06:53 +0800

    Create nsenter.sh
    new code

---
+++ nsenter.sh
@@ -0,0 +1,15 @@
+function enns() {
+    set -eu
+    ns=${2-"default"}
+    pod=`kubectl -n $ns describe pod $1 | grep -A10 "^Containers:" | grep -Eo 'docker://.*$' | head -n 1 | sed 's/docker:\/\/\(.*\)$/\1/'`
+    pid=`docker inspect -f {{.State.Pid}} $pod`
+    echo "entering pod netns for $ns/$1"
+    cmd="nsenter -n --target $pid"
+    echo $cmd
+    $cmd
+}
+
+# arg1: pod名称
+# arg2: namespace名称
+
+# enns [pod_name] [namespace_name]
